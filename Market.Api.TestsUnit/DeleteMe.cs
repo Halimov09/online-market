9d23beae03b3bf146d69abfed2aa69c7f382e77f
halimov09 <sunnatullohhalimov@gmail.com>
2024-11-15 16:37:51 +0500

    INFRA: Add Unit test

---
+++ Market.Api.TestsUnit/DeleteMe.cs
@@ -0,0 +1,13 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+namespace Market.Api.TestsUnit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBe() => Assert.True(true);
+    }
+}
