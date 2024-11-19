6d6685b1474e58bf11e3118c58bcad1955355960
halimov09 <sunnatullohhalimov@gmail.com>
2024-11-19 21:24:40 +0500

    INFRA: Add Users And Product Models

---
+++ Market.Api/Models/Foundation/Product/Product.cs
@@ -0,0 +1,17 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+namespace Market.Api.Models.Foundation.Product
+{
+    public class Product
+    {
+        //About product
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ Market.Api/Models/Foundation/Users/Role.cs
@@ -0,0 +1,14 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+namespace Market.Api.Models.Foundation.Users
+{
+    public enum Role
+    {
+        Admin,
+        Customer,
+        Vendor
+    }
+}
