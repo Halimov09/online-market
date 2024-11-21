88724f8c140daec9cbe7c97aea1468aff4e16ad2
halimov09 <sunnatullohhalimov@gmail.com>
2024-11-21 10:35:58 +0500

    INFRA: Add Brokers

---
+++ Market.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,11 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+namespace Market.Api.Brokers.Storages
+{
+    public class StorageBroker
+    {
+    }
+}
