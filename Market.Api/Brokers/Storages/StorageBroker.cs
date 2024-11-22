352c1520883bfb2ac7ac8b4784233e3ec63fd9e8
halimov09 <sunnatullohhalimov@gmail.com>
2024-11-22 16:56:59 +0500

    INFRA: Add New Codes

--- Market.Api/Brokers/Storages/StorageBroker.cs
+++ Market.Api/Brokers/Storages/StorageBroker.cs
@@ -3,9 +3,28 @@
 // Free To Use Comfort and Peace
 //==================================================
 
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+
 namespace Market.Api.Brokers.Storages
 {
-    public class StorageBroker
+    public class StorageBroker : EFxceptionsContext
     {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = this.configuration.
+                GetConnectionString(name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public override void Dispose() {}
     }
 }
