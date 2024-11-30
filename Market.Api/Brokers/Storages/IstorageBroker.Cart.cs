a4ea2731bc1f97886ad4f3dd88949777a06c8c2c
halimov09 <sunnatullohhalimov@gmail.com>
2024-11-30 17:01:18 +0500

    INFRA: Init Cart And CartItem

---
+++ Market.Api/Brokers/Storages/IstorageBroker.Cart.cs
@@ -0,0 +1,14 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Market.Api.Models.Foundation.Cart;
+
+namespace Market.Api.Brokers.Storages
+{
+    public partial interface IstorageBroker
+    {
+        ValueTask<Cart> InsertCartAsync(Cart cart);
+    }
+}
--- Market.Api/Brokers/Storages/StorageBroker.Cart.cs
+++ Market.Api/Brokers/Storages/StorageBroker.Cart.cs
@@ -5,11 +5,24 @@
 
 using Market.Api.Models.Foundation.Cart;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Market.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<Cart> carts { get; set; }
+
+        public async ValueTask<Cart> InsertCartAsync(Cart cart)
+        {
+            using var broker = new StorageBroker(this.configuration);
+        
+            EntityEntry<Cart> cartEntityEntry = 
+                await broker.carts.AddAsync(cart);
+
+            await broker.SaveChangesAsync();
+
+            return cartEntityEntry.Entity;
+        }
     }
 }
