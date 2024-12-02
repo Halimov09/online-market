8e233299a869eb444c69acc92bb3d722ba083251
halimov09 <sunnatullohhalimov@gmail.com>
2024-12-02 13:27:24 +0500

    ADD: Services File

---
+++ Market.Api/services/foundation/user/UserService.cs
@@ -0,0 +1,15 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Market.Api.Models.Foundation.Users;
+
+namespace Market.Api.services.foundation.user
+{
+    public class UserService : IuserService
+    {
+        public ValueTask<Users> AddUsersAsync() => 
+            throw new NotImplementedException();
+    }
+}
