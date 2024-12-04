8bb7302c574642dd3878b26a7ffe949dab814c0f
halimov09 <sunnatullohhalimov@gmail.com>
2024-12-04 19:25:21 +0500

    TESTS: Started Unit Tests

---
+++ Market.Api.TestsUnit/services/foundation/user/UserServiceTests.cs
@@ -0,0 +1,52 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using FluentAssertions;
+using Market.Api.Brokers.Storages;
+using Market.Api.Models.Foundation.Users;
+using Market.Api.services.foundation.user;
+using Moq;
+
+namespace Market.Api.TestsUnit.services.foundation.user
+{
+    public class UserServiceTests
+    {
+        private readonly Mock<IstorageBroker> storageBrokerMock;
+        private readonly IuserService userService;
+
+        public UserServiceTests() 
+        {
+            this.storageBrokerMock = new Mock<IstorageBroker>();
+
+            this.userService =
+                new UserService(storageBroker: this.storageBrokerMock.Object);
+        }
+
+        [Fact]
+        public async Task ShouldAddUserAsync()
+        {
+            //arrange
+            Users randomUser = new Users()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test",
+                Email = "ohef@gmail.com",
+                Password = "password",
+                Role = Role.Admin,
+                CreateDate = DateTime.Now,
+            };
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.InsertUsersAsync(randomUser))
+                .ReturnsAsync(randomUser);
+
+            //act 
+            Users actual = await this.userService.AddUsersAsync(randomUser);
+
+            //assert
+            actual.Should().BeEquivalentTo(randomUser);
+        }
+    }
+}
--- Market.Api/services/foundation/user/IuserService.cs
+++ Market.Api/services/foundation/user/IuserService.cs
@@ -3,9 +3,12 @@
 // Free To Use Comfort and Peace
 //==================================================
 
+using Market.Api.Models.Foundation.Users;
+
 namespace Market.Api.services.foundation.user
 {
     public interface IuserService
     {
+        ValueTask<Users> AddUsersAsync(Users users);
     }
 }
