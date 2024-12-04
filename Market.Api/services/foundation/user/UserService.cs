0ae60c7fba65b90ab8d43745eb8fc9a06cc08cfb
halimov09 <sunnatullohhalimov@gmail.com>
2024-12-04 16:52:42 +0500

    IMPLEMENT ISTORAGEBROKER

--- Market.Api/services/foundation/user/UserService.cs
+++ Market.Api/services/foundation/user/UserService.cs
@@ -3,12 +3,18 @@
 // Free To Use Comfort and Peace
 //==================================================
 
+using Market.Api.Brokers.Storages;
 using Market.Api.Models.Foundation.Users;
 
 namespace Market.Api.services.foundation.user
 {
     public class UserService : IuserService
     {
+        private readonly IstorageBroker storageBroker;
+
+        public UserService(IstorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+
         public ValueTask<Users> AddUsersAsync() => 
             throw new NotImplementedException();
     }
