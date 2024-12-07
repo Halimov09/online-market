4e8afb0f76990b3476277a3c1515870a43c71636
halimov09 <sunnatullohhalimov@gmail.com>
2024-12-07 18:00:46 +0500

    ShouldAddOrderAsync -> Pass

---
+++ Market.Api.TestsUnit/services/foundation/order/OrderServiceTests.Logic.Add.cs
@@ -0,0 +1,46 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Market.Api.Models.Foundation.Order;
+using Market.Api.Models.Foundation.Product;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Api.TestsUnit.services.foundation.order
+{
+    public partial class OrderServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddOrderAsync()
+        {
+            //given
+            Order randomOrder = CreateRandomOrder();
+            Order inputOrder = randomOrder;
+            Order returningOrder = inputOrder;
+            Order expectedOrder = returningOrder.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.InsertOrderAsync(inputOrder)).ReturnsAsync(returningOrder);
+
+            //when 
+            Order actualProduct =
+                await this.orderService.AddOrderAsync(inputOrder);
+
+            //then
+            actualProduct.Should().BeEquivalentTo(expectedOrder);
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.InsertOrderAsync(inputOrder), Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Market.Api.TestsUnit/services/foundation/order/OrderServiceTests.cs
@@ -0,0 +1,42 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Market.Api.Brokers.Storages;
+using Market.Api.Models.Foundation.Order;
+using Market.Api.services.foundation.order;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace Market.Api.TestsUnit.services.foundation.order
+{
+    public partial class OrderServiceTests
+    {
+        private readonly Mock<IstorageBroker> storageBrokerMock;
+        private readonly OrderService orderService;
+
+        public OrderServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IstorageBroker>();
+
+            this.orderService =
+                new OrderService(storageBroker: this.storageBrokerMock.Object);
+        }
+
+        private static Order CreateRandomOrder() =>
+            CreateOrderFiller(date: GetRandomDateTimeOffSet()).Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffSet() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Filler<Order> CreateOrderFiller(DateTimeOffset date)
+        {
+            var filler = new Filler<Order>();
+
+            filler.Setup().OnType<DateTimeOffset>().Use(date);
+
+            return filler;
+        }
+    }
+}
