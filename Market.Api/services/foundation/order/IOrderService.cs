960482f177350eff38fc0db39aa0487e48b09f79
halimov09 <sunnatullohhalimov@gmail.com>
2024-12-07 15:49:20 +0500

    ORDER: Service Completed

---
+++ Market.Api/services/foundation/order/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Market.Api.Models.Foundation.Order;
+
+namespace Market.Api.services.foundation.order
+{
+    public interface IOrderService
+    {
+        ValueTask<Order> AddOrderAsync(Order order);
+    }
+}
