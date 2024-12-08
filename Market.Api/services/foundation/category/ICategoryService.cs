46ca4dae6a3d10ae2e4e2402823a130d21dd4a93
halimov09 <sunnatullohhalimov@gmail.com>
2024-12-08 21:37:47 +0500

    CategoryServiceTests -> Pass

---
+++ Market.Api/services/foundation/category/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Market.Api.Models.Foundation.Category;
+
+namespace Market.Api.services.foundation.category
+{
+    public interface ICategoryService
+    {
+        ValueTask<Category> AddCategoryAsync(Category category);
+    }
+}
