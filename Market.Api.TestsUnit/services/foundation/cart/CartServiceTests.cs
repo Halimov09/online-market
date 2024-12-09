a92d07f98a683e35dc9169fdb28c2e234437d4d1
halimov09 <sunnatullohhalimov@gmail.com>
2024-12-09 21:42:06 +0500

    CartServiceTests -> Pass

---
+++ Market.Api.TestsUnit/services/foundation/cart/CartServiceTests.cs
@@ -0,0 +1,42 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Market.Api.Brokers.Storages;
+using Market.Api.Models.Foundation.Cart;
+using Market.Api.services.foundation.cart;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace Market.Api.TestsUnit.services.foundation.cart
+{
+    public partial class CartServiceTests
+    {
+        private readonly Mock<IstorageBroker> storageBrokerMock;
+        private readonly CartService cartService;
+
+        public CartServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IstorageBroker>();
+
+            this.cartService = 
+                new CartService(storageBroker: this.storageBrokerMock.Object);
+        }
+
+        private static Cart CreateRandomCart() =>
+            CreateCartFiller(date: GetRandomDateTimeOffSet()).Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffSet() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Filler<Cart> CreateCartFiller(DateTimeOffset date)
+        {
+            var filler = new Filler<Cart>();
+
+            filler.Setup().OnType<DateTimeOffset>().Use(date);
+
+            return filler;
+        }
+    }
+}
---
+++ Market.Api/services/foundation/cart/CartService.cs
@@ -0,0 +1,22 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Market.Api.Brokers.Storages;
+using Market.Api.Models.Foundation.Cart;
+
+namespace Market.Api.services.foundation.cart
+{
+    public class CartService : ICartService
+    {
+        private readonly IstorageBroker storageBroker;
+
+        public CartService(IstorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+
+        public async ValueTask<Cart> AddCartAsync(Cart cart) =>
+            await this.storageBroker.InsertCartAsync(cart);
+        
+    }
+}
