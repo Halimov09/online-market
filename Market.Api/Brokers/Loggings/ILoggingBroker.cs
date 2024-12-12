36a2010e9420955a8b641d06812d9107c55e2655
halimov09 <sunnatullohhalimov@gmail.com>
2024-12-12 22:26:29 +0500

    ILoggingBroker -> Implement

---
+++ Market.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,13 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+namespace Market.Api.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
