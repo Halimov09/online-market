3ab74d9ec21b78d82a775c455c1df0c52848fe30
halimov09 <sunnatullohhalimov@gmail.com>
2024-12-12 08:59:29 +0500

    ShouldThrowValidationExceptionOnAddIfUserNullAndLogitAsync -> WIP

---
+++ Market.Api.TestsUnit/services/foundation/user/UserServiceTests.Validation.cs
@@ -0,0 +1,32 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Market.Api.Models.Foundation.Users;
+using Market.Api.Models.Foundation.Users.exceptions;
+
+namespace Market.Api.TestsUnit.services.foundation.user
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfUserNullAndLogitAsync()
+        {
+            //given
+            Users nullUser = null;
+            var nullGuestException = new NullUserException();
+
+            var expectedUserValidationException = 
+                new UserValidationExcption(nullGuestException);
+
+            //when
+            ValueTask<Users> addUserTask = 
+                this.userService.AddUsersAsync(nullUser);
+
+            //then
+            await Assert.ThrowsAsync<UserValidationExcption>(() =>
+            addUserTask.AsTask());
+        }
+    }
+}
