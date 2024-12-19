ab628f81a1bebe8d56554b9b1ca2a080492d36e2
Sunnatulloh <100553464+Halimov09@users.noreply.github.com>
2024-12-19 23:12:48 +0500

    Revert "ShouldThrowExceptionOnAddIfPaymentNullAndLogitAsync -> Pass"

--- Market.Api/services/foundation/payment/PaymentService.cs
+++ Market.Api/services/foundation/payment/PaymentService.cs
@@ -3,47 +3,19 @@
 // Free To Use Comfort and Peace
 //==================================================
 
-using Market.Api.Brokers.Loggings;
 using Market.Api.Brokers.Storages;
 using Market.Api.Models.Foundation.Payment;
-using Market.Api.Models.Foundation.Payment.exception;
-using Xeptions;
 
 namespace Market.Api.services.foundation.payment
 {
     public class PaymentService : IpaymentService
     {
         private readonly IstorageBroker storageBroker;
-        private readonly ILoggingBroker loggingBroker;
 
-        public PaymentService(
-            IstorageBroker storageBroker, 
-            ILoggingBroker loggingBroker)
-        {
+        public PaymentService(IstorageBroker storageBroker) =>
             this.storageBroker = storageBroker;
-            this.loggingBroker = loggingBroker;
-        }
 
-        public async ValueTask<Payment> AddPaymentAsync(Payment payment)
-        {
-            try
-            {
-                if (payment is null)
-                {
-                    throw new NullPaymentException();
-                }
-                return await this.storageBroker.InsertPaymentAsync(payment);
-            }
-            catch (NullPaymentException nullPaymentException) 
-            {
-                var paymentNullException =
-                    new PaymentValidationException(nullPaymentException);
-
-                this.loggingBroker.LogError(paymentNullException);
-
-                throw paymentNullException;
-            }
-
-        }
+        public async ValueTask<Payment> AddPaymentAsync(Payment payment)=>
+            await this.storageBroker.InsertPaymentAsync(payment);
     }
 }
