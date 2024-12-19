36749e075a84a6d731d215b80288a31fae6ff216
halimov09 <sunnatullohhalimov@gmail.com>
2024-12-19 22:06:32 +0500

    ShouldThrowExceptionOnAddIfPaymentNullAndLogitAsync -> Pass

---
+++ Market.Api.TestsUnit/services/foundation/payment/PaymentServiceTests.Validation.cs
@@ -0,0 +1,44 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Market.Api.Models.Foundation.Payment;
+using Market.Api.Models.Foundation.Payment.exception;
+using Moq;
+using Xunit.Sdk;
+
+namespace Market.Api.TestsUnit.services.foundation.payment
+{
+    public partial class PaymentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowExceptionOnAddIfPaymentNullAndLogitAsync()
+        {
+            //given
+            Payment paymentNull = null;
+            var paymetNullException = new NullPaymentException();
+
+            var expectedNullPaymentException = 
+                new PaymentValidationException(paymetNullException);
+
+            //when
+            ValueTask<Payment> addPaymentTask =
+                this.paymentService.AddPaymentAsync(paymentNull);
+
+            //then
+            await Assert.ThrowsAsync<PaymentValidationException> (() =>
+            addPaymentTask.AsTask ());
+
+            this.loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(expectedNullPaymentException))), 
+            Times.Once());
+
+            this.storageBrokerMock.Verify(broker => 
+            broker.InsertPaymentAsync(It.IsAny<Payment>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Market.Api/Models/Foundation/Payment/exception/PaymentValidationException.cs
@@ -0,0 +1,17 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Xeptions;
+
+namespace Market.Api.Models.Foundation.Payment.exception
+{
+    public class PaymentValidationException : Xeption
+    {
+        public PaymentValidationException(Xeption innerPaymentExeption)
+            :base(message: "Payment validation error occured, fix the error please try again",
+                 innerPaymentExeption)
+        {}
+    }
+}
