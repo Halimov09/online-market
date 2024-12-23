c4e27f67e0c3c8b7a6e883955507cc86e282b805
halimov09 <sunnatullohhalimov@gmail.com>
2024-12-23 20:26:49 +0500

    CODE-RUB: Impliment Try-Catch

---
+++ Market.Api/services/foundation/user/UserService.Validation.cs
@@ -0,0 +1,21 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Market.Api.Models.Foundation.Users;
+using Market.Api.Models.Foundation.Users.exceptions;
+
+namespace Market.Api.services.foundation.user
+{
+    public partial class UserService
+    {
+        private void ValidateUserNotNull(Users users)
+        {
+            if (users is null)
+            {
+                throw new NullUserException();
+            }
+        }
+    }
+}
