97e32e473f2d98e08f82df9d3ae0574f8d282868
halimov09 <sunnatullohhalimov@gmail.com>
2024-12-23 11:10:44 +0500

    ShouldThrowValidationCartOnAddIfAndLogitAsync -> Pass

--- Market.Api.TestsUnit/services/foundation/cart/CartServiceTests.cs
+++ Market.Api.TestsUnit/services/foundation/cart/CartServiceTests.cs
@@ -3,29 +3,47 @@
 // Free To Use Comfort and Peace
 //==================================================
 
+using Market.Api.Brokers.Loggings;
 using Market.Api.Brokers.Storages;
 using Market.Api.Models.Foundation.Cart;
+using Market.Api.Models.Foundation.Cart.exception;
 using Market.Api.services.foundation.cart;
 using Moq;
+using System.Linq.Expressions;
 using Tynamix.ObjectFiller;
+using Xeptions;
 
 namespace Market.Api.TestsUnit.services.foundation.cart
 {
     public partial class CartServiceTests
     {
         private readonly Mock<IstorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;    
         private readonly CartService cartService;
 
         public CartServiceTests()
         {
             this.storageBrokerMock = new Mock<IstorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
 
             this.cartService = 
-                new CartService(storageBroker: this.storageBrokerMock.Object);
+                new CartService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
         }
 
         private static Cart CreateRandomCart() =>
             CreateCartFiller(date: GetRandomDateTimeOffSet()).Create();
+
+        public Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedCArtException)
+        {
+            return actualCArtException => 
+            actualCArtException.Message == expectedCArtException.Message &&
+            actualCArtException.InnerException.Message ==
+            expectedCArtException.InnerException.Message 
+            && (actualCArtException.InnerException as Xeption)
+            .DataEquals(expectedCArtException.Data);
+        }
 
         private static DateTimeOffset GetRandomDateTimeOffSet() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
