14651c562249b96ffd9e5286ee455317854e413c
halimov09 <sunnatullohhalimov@gmail.com>
2025-01-01 23:52:21 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccurceAndLogItAsync -> Continues

---
+++ Market.Api.TestsUnit/services/foundation/user/UserServiceTests.Exception.cs
@@ -0,0 +1,25 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Market.Api.Models.Foundation.Users;
+using Microsoft.Data.SqlClient;
+
+namespace Market.Api.TestsUnit.services.foundation.user
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccurceAndLogItAsync()
+        {
+            //given
+            Users someUser = CreateRandomUser();
+            SqlException sqlException = GetSqlError();
+
+            //when
+
+            //then
+        }
+    }
+}
