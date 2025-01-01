8545a8fff603a85abc7b90b9185b82143d67fa78
halimov09 <sunnatullohhalimov@gmail.com>
2025-01-02 00:21:19 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccurceAndLogItAsync -> Fail

--- Market.Api.TestsUnit/services/foundation/user/UserServiceTests.Exception.cs
+++ Market.Api.TestsUnit/services/foundation/user/UserServiceTests.Exception.cs
@@ -4,7 +4,9 @@
 //==================================================
 
 using Market.Api.Models.Foundation.Users;
+using Market.Api.Models.Foundation.Users.exceptions;
 using Microsoft.Data.SqlClient;
+using Moq;
 
 namespace Market.Api.TestsUnit.services.foundation.user
 {
@@ -16,10 +18,32 @@ namespace Market.Api.TestsUnit.services.foundation.user
             //given
             Users someUser = CreateRandomUser();
             SqlException sqlException = GetSqlError();
+            var failedUserStorageException = new FailedUserStorageException(sqlException);
+
+            var expectedUserException =
+                new UserDependencyException(failedUserStorageException);
+
+            this.storageBrokerMock.Setup(broker => 
+            broker.InsertUsersAsync(someUser))
+                .ThrowsAsync(sqlException);
 
             //when
+            ValueTask<Users> addUserTask =
+                this.userService.AddUsersAsync(someUser);
 
             //then
+            await Assert.ThrowsAsync<UserDependencyException> (() =>
+            addUserTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.InsertUsersAsync(someUser), Times.Once());
+
+            this.loggingBrokerMock.Verify(broker => 
+            broker.LogCritical(It.Is(SameExceptionAs(expectedUserException))),
+            Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
---
+++ Market.Api/Models/Foundation/Users/exceptions/FailedUserStorageException.cs
@@ -0,0 +1,17 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Xeptions;
+
+namespace Market.Api.Models.Foundation.Users.exceptions
+{
+    public class FailedUserStorageException : Xeption
+    {
+        public FailedUserStorageException(Exception innerException)
+            :base("Failed user storage error occured, contact support",
+                 innerException)
+        {}
+    }
+}
